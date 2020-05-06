e2d360505d5b87b60f4fc732c2238005679347f2
Max <you@example.com>
2020-05-06 12:31:57 +0200

    new c file

--- Week3/opgave3_1.c
+++ Week3/opgave3_1.c
@@ -1,14 +1,49 @@
 #include <linux/init.h>
 #include <linux/module.h>
 MODULE_LICENSE("Dual BSD/GPL");
-static int hello_init(void){
-    printk(KERN_ALERT "Hello, world\n");
+
+
+struct file_operations fileOps = {
+        .read = dev_read,
+        .write = dev_write,
+        .open = dev_open,
+        .release = dev_release,
+        };
+
+
+ssize_t dev_read(struct file *filp, char *buffer, size_t len, loff_t *offset){
+    printk(KERN_ALERT "Read %ld?\n",len);
+    return 1;
+}
+
+ssize_t dev_write(struct file *filp, char *buffer, size_t len, loff_t *offset){
+    printk(KERN_ALERT "Write %ld?\n",len);
+    return 1;
+}
+
+int dev_open(struct inode *inode, struct file *filp) {
+    printk(KERN_ALERT "Opened device\n");
+
+    return 0;
+}
+
+int dev_release(struct inode *inode, struct file *filp) {
+    printk(KERN_ALERT "Opened device\n");
+
+    return 0;
+}
+
+static int dev_init(void){
+    printk(KERN_ALERT "init device\n");
+
+//    cdev = cdev_alloc();
+
     return 0;
     }
-static void hello_exit(void){
+static void dev_exit(void){
     printk(KERN_ALERT "Goodbye, world\n");
     }
     
     
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(dev_init);
+module_exit(dev_exit);
